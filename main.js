223a99bcf4e608f11482c40a5ce51635fe460f76
Tobías Silva <tobisilva4225@gmail.com>
2023-11-16 12:30:58 -0300

    cambios de estilos, cambios en js

--- main.js
+++ main.js
@@ -1,74 +1,108 @@
-//Crear un sistema de reservas para un resto en el cual tengo disponibles tres salones (ABAJO, ARRIBA; PATIO)
+//Crear un sistema de reservas para un resto en el cual tengo disponibles tres salones (ABAJO, ARRIBA, PATIO)
 //Tengo dos turnos, Uno 20hs y otro 22hs
 //SALON ABAJO: Mesas de 4(4), Mesas de 2(6).
 //SALON ARRIBA: Mesas de 2(6), Mesa de 8(1).
 //SALON PATIO: Mesas de 2(4), Mesas de 4(2).
-//solicitar nombre de la reserva, numero de comenzales, dia y hora
-function reserva() {
-  let nombre = prompt("Ingresa tu nombre!");
-  let numeroPersonas = parseInt(prompt("¿Cuantos comenzales serán?"));
-  let dia = prompt("Ingresar dia");
-  let hora = prompt("¿Turno 20hs o 22hs?");
-  if (numeroPersonas === 1 && nombre != "") {
+//solicitar nombre de la reserva, numero de comenzales, dia, hora y telefono.
+
+// Constructor para la Reserva
+function Reserva(nombre, hora, personas, dia, fecha, telefono) {
+  this.nombre = nombre;
+  this.hora = hora;
+  this.personas = personas;
+  this.dia = dia;
+  this.fecha = fecha;
+  this.telefono = telefono;
+}
+
+// Array para almacenar todas las reservas
+let reservas = [];
+
+// Función para crear una nueva reserva
+function crearReserva(nombre, hora, personas, dia, fecha, telefono) {
+  let nuevaReserva = new Reserva(nombre, hora, personas, dia, fecha, telefono);
+  reservas.push(nuevaReserva);
+
+  if (personas === 1 && nombre != "") {
     switch (dia) {
       case "domingo":
-        alert("¡estaremos cerrados!");
+        alert("¡El domingo estaremos cerrados!");
         break;
       case "lunes":
-        alert("¡estaremos cerrados!");
+        alert("¡El lunes estaremos cerrados!");
         break;
       default:
         alert(`¡Perfecto ${nombre}, Te esperamos el ${dia} a las ${hora}!`);
         break;
     }
-  } else if (numeroPersonas < 10 && nombre != "") {
+  } else if (personas < 10 && nombre != "") {
     switch (dia) {
       case "domingo":
-        alert("¡estaremos cerrados!");
+        alert("¡El domingo estaremos cerrados!");
         break;
       case "lunes":
-        alert("¡estaremos cerrados!");
+        alert("¡El lunes estaremos cerrados!");
         break;
       default:
         alert(`¡Perfecto ${nombre}, Los esperamos el ${dia} a las ${hora}!`);
         break;
     }
-  } else if (numeroPersonas >= 10) {
-    alert("Consultar disponibilidad.");
+  } else if (personas >= 10) {
+    alert("Para reservas mayores a 10 comenzales, consultar disponibilidad.");
   } else {
-    alert("Ingresar valores validos");
+    alert("Corroborar que los campos solicitados esten llenos y bien escritos");
   }
 }
 
-let reservas = [
-  { nombre: "Juan", hora: "20hs", personas: 5 },
-  { nombre: "Maria", hora: "20hs", personas: 3 },
-  { nombre: "Fernando", hora: "22hs", personas: 2 },
-  { nombre: "Agustin", hora: "20hs", personas: 6 },
-  { nombre: "Gabriel", hora: "22hs", personas: 2 },
-];
+// Función para mostrar todas las reservas
 
-for (let i = 0; i < reservas.length; i++) {
-  let reserva = reservas[i];
-  console.log(
-    `Reserva a nombre de ${reserva.nombre} a las ${reserva.hora} para ${reserva.personas} personas`
-  );
-}
-/* 
-reserva(); */
-
-function Reserva(nombre, hora, personas) {
-  this.nombre = nombre;
-  this.hora = hora;
-  this.personas = personas;
-  this.mensaje = function () {
-    console.log("Hola " + nombre + " te esperamos a las " + hora);
-  };
+function mostrarReservas() {
+  for (let i = 0; i < reservas.length; i++) {
+    console.log(
+      "Reserva a nombre de " +
+        reservas[i].nombre +
+        " para las " +
+        reservas[i].hora +
+        " el dia " +
+        reservas[i].dia +
+        " " +
+        reservas[i].fecha +
+        " para " +
+        reservas[i].personas +
+        " personas." +
+        " Telefono: " +
+        reservas[i].telefono
+    );
+  }
 }
 
-const reserva1 = new Reserva("juan", "20hs", 5);
-const reserva2 = new Reserva("maria", "20hs", 3);
-const reserva3 = new Reserva("fernando", "22hs", 2);
-const reserva4 = new Reserva("agustin", "20hs", 6);
+// Crear reservas
 
-console.log(reserva1.mensaje());
+crearReserva(
+  prompt("ingresar nombre y apellido"),
+  prompt("¿Turno 20hs o 22hs? (ingresar tal cual aparece en este mensaje)"),
+  parseInt(prompt("¿Cuantos comenzales serán? ingresar numeros")),
+  prompt("Ingresar dia (lunes, martes...)"),
+  prompt("Ingresar fecha (ejemplo 02/10)"),
+  prompt("Ingresar numero de telefono")
+);
+
+crearReserva(
+  prompt("ingresar nombre y apellido"),
+  prompt("¿Turno 20hs o 22hs? (ingresar tal cual aparece en este mensaje)"),
+  parseInt(prompt("¿Cuantos comenzales serán? ingresar numeros")),
+  prompt("Ingresar dia (lunes, martes...)"),
+  prompt("Ingresar fecha (ejemplo 02/10)"),
+  prompt("Ingresar numero de telefono")
+);
+crearReserva(
+  prompt("ingresar nombre y apellido"),
+  prompt("¿Turno 20hs o 22hs? (ingresar tal cual aparece en este mensaje)"),
+  parseInt(prompt("¿Cuantos comenzales serán? ingresar numeros")),
+  prompt("Ingresar dia (lunes, martes...)"),
+  prompt("Ingresar fecha (ejemplo 02/10)"),
+  prompt("Ingresar numero de telefono")
+);
+
+// Mostrar todas las reservas
+mostrarReservas();
